9cde3049946788320a9a72addeaa5e3ab15a979b
zplot <luis2@zplot.com>
2016-01-16 11:44:44 +0100

    save

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -27,14 +27,13 @@ object Application extends Controller {
 
   def hopf1 = Action {
     val t5 = Tree(List())
-    println(t5)
+
     val t6 = Tree(List(t5, t5, t5))
-    println(t6)
+
     val t7 = Tree(List(t5, t6, t5))
-    println("t7 = " + t7)
-    println(t7.weight)
+
     val t8 = Tree.string2Tree(t7.toString)
-    println("t8 = " + t8)
+
     Ok(views.html.hopf1(t8.toString))
   }
 
